0c21e3b5655377b71ba3974c5f709bb9fd7eab79
sqspguru <sqspguru@gmail.com>
2025-12-27 01:50:11 +0500

    Update javascript.js

--- TextZoom/javascript.js
+++ TextZoom/javascript.js
@@ -1,1 +1,121 @@
-!function(t){t.fn.fontZoomer=function(o){var e=t.extend({step:10,minZoom:80,maxZoom:150,container:"html",position:"right",useCookies:!0,cookieName:"fontZoom",cookieDays:30,tooltips:!0,scope:"site"},o);if("blog"===e.scope){var n=t(".blog-basic-grid-item-wrapper .sqs-block-content, .blog-item-wrapper");if(0===n.length)return this;e.container=n}var i,s,a=100;if(e.useCookies){var l=(i=e.cookieName,2===(s=("; "+document.cookie).split("; "+i+"=")).length?s.pop().split(";").shift():null);l&&(a=parseInt(l))}else localStorage.getItem("fontZoom")&&(a=parseInt(localStorage.getItem("fontZoom")));var r=e.tooltips?"tooltip-enabled":"",c=t('<div class="fontzoom-controls fontzoom-floating '+r+'"><button class="fz-toggle" title="Font Options"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 23" width="36" height="23"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-linejoin="round" d="M26.58 21.3225806V1m-7.92 4.06451613V1H34.5v4.06451613"/><path d="M22.62 21.3225806h7.92"/><path stroke-linejoin="round" d="M6.78 18.6129032V5.06451613M1.5 7.77419355V5.06451613h10.56v2.70967742"/><path d="M4.14 18.6129032h5.28"/></g></svg></button><div class="fontzoom-buttons"><button class="fz-in" data-tooltip="Increase Font Size">A+</button><button class="fz-reset" data-tooltip="Reset Font Size">A</button><button class="fz-out" data-tooltip="Decrease Font Size">A-</button></div></div>');function f(){if(t(window).width()>767)switch(e.position){case"left":c.css({top:"50%",left:"20px",bottom:"auto",right:"auto",transform:"translateY(-50%)"}),c.find("button").addClass("tooltip-right");break;case"right":c.css({top:"50%",right:"20px",bottom:"auto",left:"auto",transform:"translateY(-50%)"}),c.find("button").addClass("tooltip-left");break;default:c.css({bottom:"20px",right:"20px",top:"auto",left:"auto",transform:"none"}),c.find("button").addClass("tooltip-left")}else c.css({bottom:"20px",right:"20px",top:"auto",left:"auto",transform:"none"})}function p(){t(e.container).css("font-size",a+"%"),e.useCookies?function(t,o,e){var n=new Date;n.setTime(n.getTime()+24*e*60*60*1e3),document.cookie=t+"="+o+";path=/;expires="+n.toUTCString()}(e.cookieName,a,e.cookieDays):localStorage.setItem("fontZoom",a)}return t("body").append(c),f(),t(window).on("resize",f),c.on("click",".fz-in",function(){a=Math.min(a+e.step,e.maxZoom),p()}),c.on("click",".fz-out",function(){a=Math.max(a-e.step,e.minZoom),p()}),c.on("click",".fz-reset",function(){a=100,p()}),c.on("click",".fz-toggle",function(o){t(window).width()<=767&&(o.stopPropagation(),c.toggleClass("expanded"))}),c.on("click",".fontzoom-buttons button",function(t){t.stopPropagation()}),t(document).on("click",function(){t(window).width()<=767&&c.removeClass("expanded")}),p(),this}}(jQuery);
+(function ($) {
+    $.fn.fontZoomer = function (options) {
+        var settings = $.extend({
+            step: 10,
+            minZoom: 80,
+            maxZoom: 150,
+            container: "html",
+            position: "right", // 'left', 'right', 'bottom-right'
+            useCookies: true,
+            cookieName: "fontZoom",
+            cookieDays: 30,
+            tooltips: true,
+            scope: "site" // 'site' or 'blog'
+        }, options);
+
+        // If blog scope, use blog container
+        if (settings.scope === "blog") {
+            var $blogContainer = $(".blog-basic-grid-item-wrapper .sqs-block-content, .blog-item-wrapper");
+            if ($blogContainer.length === 0) return this;
+            settings.container = $blogContainer;
+        }
+
+        // Cookie helpers
+        function setCookie(name, value, days) {
+            var d = new Date();
+            d.setTime(d.getTime() + (days*24*60*60*1000));
+            document.cookie = name + "=" + value + ";path=/;expires=" + d.toUTCString();
+        }
+
+        function getCookie(name) {
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            if(parts.length === 2) return parts.pop().split(";").shift();
+            return null;
+        }
+
+        // Load saved zoom
+        var currentZoom = 100;
+        if (settings.useCookies) {
+            var savedZoom = getCookie(settings.cookieName);
+            if (savedZoom) currentZoom = parseInt(savedZoom);
+        } else if (localStorage.getItem("fontZoom")) {
+            currentZoom = parseInt(localStorage.getItem("fontZoom"));
+        }
+
+        // Toolbar SVG
+        var toggleSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 23" width="36" height="23"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-linejoin="round" d="M26.58 21.3225806V1m-7.92 4.06451613V1H34.5v4.06451613"/><path d="M22.62 21.3225806h7.92"/><path stroke-linejoin="round" d="M6.78 18.6129032V5.06451613M1.5 7.77419355V5.06451613h10.56v2.70967742"/><path d="M4.14 18.6129032h5.28"/></g></svg>';
+
+        var tooltipClass = settings.tooltips ? 'tooltip-enabled' : '';
+
+        // Create toolbar
+        var $controls = $('<div class="fontzoom-controls fontzoom-floating '+tooltipClass+'">' +
+            '<button class="fz-toggle" title="Font Options">' + toggleSVG + '</button>' +
+            '<div class="fontzoom-buttons">' +
+            '<button class="fz-in" data-tooltip="Increase Font Size">A+</button>' +
+            '<button class="fz-reset" data-tooltip="Reset Font Size">A</button>' +
+            '<button class="fz-out" data-tooltip="Decrease Font Size">A-</button>' +
+            '</div></div>');
+
+        $('body').append($controls);
+
+        // Position toolbar
+        function setPosition() {
+            if ($(window).width() > 767) {
+                switch(settings.position) {
+                    case 'left':
+                        $controls.css({top:'50%', left:'20px', bottom:'auto', right:'auto', transform:'translateY(-50%)'});
+                        $controls.find('button').addClass('tooltip-right');
+                        break;
+                    case 'right':
+                        $controls.css({top:'50%', right:'20px', bottom:'auto', left:'auto', transform:'translateY(-50%)'});
+                        $controls.find('button').addClass('tooltip-left');
+                        break;
+                    case 'bottom-right':
+                    default:
+                        $controls.css({bottom:'20px', right:'20px', top:'auto', left:'auto', transform:'none'});
+                        $controls.find('button').addClass('tooltip-left');
+                        break;
+                }
+            } else {
+                $controls.css({bottom:'20px', right:'20px', top:'auto', left:'auto', transform:'none'});
+            }
+        }
+        setPosition();
+        $(window).on('resize', setPosition);
+
+        // Update zoom
+        function updateZoom() {
+            $(settings.container).css('font-size', currentZoom + '%');
+            if(settings.useCookies){
+                setCookie(settings.cookieName, currentZoom, settings.cookieDays);
+            } else {
+                localStorage.setItem('fontZoom', currentZoom);
+            }
+        }
+
+        // Button handlers
+        $controls.on('click', '.fz-in', function(){ currentZoom = Math.min(currentZoom + settings.step, settings.maxZoom); updateZoom(); });
+        $controls.on('click', '.fz-out', function(){ currentZoom = Math.max(currentZoom - settings.step, settings.minZoom); updateZoom(); });
+        $controls.on('click', '.fz-reset', function(){ currentZoom = 100; updateZoom(); });
+
+        // Mobile toggle
+        $controls.on('click', '.fz-toggle', function(e){
+            if($(window).width() <= 767){
+                e.stopPropagation();
+                $controls.toggleClass('expanded');
+            }
+        });
+        $controls.on('click', '.fontzoom-buttons button', function(e){ e.stopPropagation(); });
+
+        // Close toolbar on outside click (mobile)
+        $(document).on('click', function(){
+            if($(window).width() <= 767) $controls.removeClass('expanded');
+        });
+
+        // Initial zoom
+        updateZoom();
+
+        return this;
+    };
+})(jQuery);
